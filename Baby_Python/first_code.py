23058e464633e2217040ce355d582164359a3fe0
unknown <catlett.rc@gmail.com>
2015-09-08 20:09:24 +0000

    First Python stuff

---
+++ Baby_Python/first_code.py
@@ -0,0 +1,9 @@
+for item in range(101): 
+	print ("Item: " + str(item))
+	print("Item Again: " + str(item))
+	
+i = 0
+ending = 100
+while i <= ending: 
+	print ("The value of i is: " + str(i))
+	i = i + 1
