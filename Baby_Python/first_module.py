2881f77f6b2fd6be64eeb7e3b0309e9fa22a5c3e
rcatlett <catlett.rc@gmail.com>
2015-09-08 20:49:26 -0400

    Finish Hist function

--- Baby_Python/first_module.py
+++ Baby_Python/first_module.py
@@ -7,5 +7,10 @@ def optional_args_test(a, b, *rest):
 	print("Rest: " + str(rest))
 	
 def hist(elements):
+	h = {}
 	for e in elements:
-		
+		if (h.has_key(e) == False):
+			h[e] = 1
+		else:
+			h[e] = h[e] + 1
+	return h					
---
+++ Baby_Python/hist.py
@@ -0,0 +1,4 @@
+import first_module as fm
+
+h = fm.hist([1,2,3,1,2,3])
+print(h)
